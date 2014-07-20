b3448ded43e7b874ff731a2b373326980258c8ee
MaxGfeller <max.gfeller@gmail.com>
2014-07-20 21:39:54 +0200

    Adapt to api changes

--- tests/test-giffer.js
+++ tests/test-giffer.js
@@ -1,16 +1,26 @@
 var test = require('tap').test
+var levelup = require('levelup')
 var Giffer = require('../')
+var TestAdapter = require('./testadapter')
+
+var db = levelup('/whatever', {
+    db: require('memdown')
+})
 
 test('Test basic functionality of giffer', function(t) {
+    var testAdapter = new TestAdapter()
+
     var giffer = new Giffer({
-        db: __dirname + '/temp/db',
-        outputDir: __dirname + '/temp'
+        db: db,
+        outputDir: __dirname + '/temp',
+        adapters: [testAdapter]
     })
 
     giffer.start()
-    giffer.on('gif', function(filename) {
+    giffer.on('gif', function(url) {
+        console.log('url', url)
         giffer.stop()
-        t.ok(filename)
+        t.ok(url)
         t.end()
     })
 })
