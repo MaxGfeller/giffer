226cf4e092b5a8dd23aa63ff86318fefdc9670a1
Gordon Mickel <gm@cyon.ch>
2014-08-03 15:43:16 +0200

    Create thumbnails from created gifs

--- tests/test-giffer.js
+++ tests/test-giffer.js
@@ -1,25 +1,26 @@
-var test = require('tap').test
-var levelup = require('levelup')
-var Giffer = require('../')
-var TestAdapter = require('./testadapter')
+var test = require('tap').test;
+var levelup = require('levelup');
+var Giffer = require('../');
+var TestAdapter = require('./testadapter');
 
 var db = levelup('/whatever', {
     db: require('memdown')
-})
+});
 
 test('Test basic functionality of giffer', function(t) {
-    var testAdapter = new TestAdapter()
+    var testAdapter = new TestAdapter();
 
     var giffer = new Giffer({
         db: db,
         outputDir: __dirname + '/temp',
+        thumbDir: 'thumbs',
         adapters: [testAdapter]
-    })
+    });
 
-    giffer.start()
+    giffer.start();
     giffer.on('gif', function(url) {
-        giffer.stop()
-        t.ok(url)
-        t.end()
-    })
-})
+        giffer.stop();
+        t.ok(url);
+        t.end();
+    });
+});
