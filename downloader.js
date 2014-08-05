fac6407a664679c84422e9d4535512ef8fd5492d
MaxGfeller <max.gfeller@gmail.com>
2014-08-05 20:27:50 +0200

    Make sure to only download one gif at a time

--- downloader.js
+++ downloader.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 
 function Downloader() {
     this.queue = []
+    this.downloading = false
 }
 
 Downloader.prototype.download = function(url, path, cb) {
@@ -12,11 +13,14 @@ Downloader.prototype.download = function(url, path, cb) {
         cb: cb
     })
 
-    if(this.queue.length === 1) this._processNextItem()
+    if(this.queue.length === 1 && this.downloading === false)
+        this._processNextItem()
 }
 
 Downloader.prototype._processNextItem = function() {
     if(this.queue.length === 0) return
+
+    this.downloading = true
 
     var obj = this.queue.shift()
     var url = obj.url
@@ -25,6 +29,7 @@ Downloader.prototype._processNextItem = function() {
 
     needle.get(url).pipe(fs.createWriteStream(path))
         .on('close', function() {
+            this.downloading = false
             this._processNextItem()
             return cb()
         }.bind(this))
